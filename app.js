73d1d93efd48cde037fb00a0f1bc6a9c008f166e
KunalPV <kpv904@gmail.com>
2021-09-18 00:22:12 +0530

    Added port

--- app.js
+++ app.js
@@ -67,6 +67,6 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cak2f.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then((result) => {
-    app.listen(8080);
+    app.listen(process.env.PORT || 8080);
   })
   .catch((err) => console.log(err));
